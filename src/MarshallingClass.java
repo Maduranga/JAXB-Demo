ebaeea45a2048a2455bde684936c4a4a32934a57
Maduranga Liyanage <maduranga.liyanage@gmail.com>
2011-12-13 23:47:57 +0900

    second commit

--- src/MarshallingClass.java
+++ src/MarshallingClass.java
@@ -10,12 +10,12 @@ public class MarshallingClass {
 	public static void main(String args[]) throws Exception
 	{
 		Person personInstance = new Person();
-		personInstance.name = "Maduranga";
-		personInstance.age = 30;
-		personInstance.company = "SLB";
-		personInstance.records.add("Intern");
-		personInstance.records.add("Engineer");
-		personInstance.records.add("Project leader");
+		personInstance.name = "Duffy Duck";
+		personInstance.age = 6;
+		personInstance.company = "Disney";
+		personInstance.records.add("prototype");
+		personInstance.records.add("Experimental");
+		personInstance.records.add("commercialized");
 		
 		final JAXBContext jaxbContext = JAXBContext.newInstance(Person.class);
 		Marshaller marsh = jaxbContext.createMarshaller();
